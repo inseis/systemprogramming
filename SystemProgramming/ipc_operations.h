d65118f4a900ef12ce3ead828580e2b839f24b79
inseis <130066407+inseis@users.noreply.github.com>
2024-06-12 04:37:24 +0900

    202003215_시스템프로그래밍평과과제
    이 프로젝트는 다양한 시스템 프로그래밍 기술을 사용하여 백업 시스템을 구현하는 예제입니다.

---
+++ SystemProgramming/ipc_operations.h
@@ -0,0 +1,9 @@
+#ifndef IPC_OPERATIONS_H
+#define IPC_OPERATIONS_H
+
+void pipe_example();
+void message_queue_example(const char *message);
+void shared_memory_example(const char *message);
+
+#endif // IPC_OPERATIONS_H
+
