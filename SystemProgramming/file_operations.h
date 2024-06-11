7130a1340038d00673e17f16e2704f560b3a4448
inseis <130066407+inseis@users.noreply.github.com>
2024-06-12 04:40:50 +0900

    Delete SystemProgramming directory

--- SystemProgramming/file_operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_OPERATIONS_H
-#define FILE_OPERATIONS_H
-
-void list_directory(const char *dir_path);
-void file_io_example();
-void print_file_info(const char *filename);
-
-#endif // FILE_OPERATIONS_H
-
--- SystemProgramming/ipc_operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef IPC_OPERATIONS_H
-#define IPC_OPERATIONS_H
-
-void pipe_example();
-void message_queue_example(const char *message);
-void shared_memory_example(const char *message);
-
-#endif // IPC_OPERATIONS_H
-
